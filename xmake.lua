8c8d4b46eaa91970c3303904365cf6ecd33cd6b8
Mrowr Purr <mrowr.purr@gmail.com>
2026-04-17 15:54:13 -0700

    fix add_headerfiles

--- xmake.lua
+++ xmake.lua
@@ -3,5 +3,5 @@ set_version("3.12.0")
 
 target("nlohmann_json")
     set_kind("headeronly")
-    add_headerfiles("include/**.hpp")
+    add_headerfiles("include/(**.hpp)")
     add_includedirs("include", { public = true })
