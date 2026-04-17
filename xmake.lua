f46eecbf4c68196a3fa225489d9331f3bb001450
Mrowr Purr <mrowr.purr@gmail.com>
2026-04-17 12:19:16 -0700

    xmake-ify

---
+++ xmake.lua
@@ -0,0 +1,7 @@
+set_project("nlohmann_json")
+set_version("3.12.0")
+
+target("nlohmann_json")
+    set_kind("headeronly")
+    add_headerfiles("include/**.hpp")
+    add_includedirs("include", { public = true })
